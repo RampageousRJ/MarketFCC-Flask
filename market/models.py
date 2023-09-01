def1f9b11e370668e6784dd2651dc43d5f0b4483
Rishabh Jain <rishabh.jain1@learner.manipal.edu>
2023-09-01 23:17:26 +0530

    Modals Implemented

--- market/models.py
+++ market/models.py
@@ -12,7 +12,7 @@ class User(db.Model,UserMixin):
     username=db.Column(db.String(length=30),nullable=False,unique=True)
     email=db.Column(db.String(length=50),nullable=False,unique=True)
     password_hash=db.Column(db.String(length=60),nullable=False)
-    budget=db.Column(db.Integer,nullable=False,default=1000)
+    budget=db.Column(db.Integer,nullable=False,default=20000)
     items = db.relationship('Item',backref='owner')
     
     def __repr__(self):
